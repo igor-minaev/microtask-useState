abeed1d9a2dfba2f80ff345edd50d1ab6f903775
Igor Minayeu <igor.minayeu@gmail.com>
2023-09-27 20:19:54 +0300

    start microtask-useState

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    let a = 1
+    return (
+        <div className="App">
+            <h1>{a}</h1>
+        </div>
+    );
+}
+
+export default App;
