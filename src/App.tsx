02eda964b0b476d64be4301c18a4ea9d425a9395
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-24 17:24:21 +0300

    microtask useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    let [a, setA] = useState(1)
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
+    }
+    const resetHandler = () => {
+        setA(0)
+        console.log(a)
+    }
     return (
         <div className="App">
-
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={resetHandler}>0</button>
         </div>
     );
 }
